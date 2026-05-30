85935e3462ea534bfe0c67521b5769d992728769
Enzo Teves <enzoalvesteves@gmail.com>
2026-05-30 14:21:51 -0300

    conferido client ms-usuarios

--- app/src/main/java/com/example/curriculumapp/client/usuario/AuthClient.kt
+++ app/src/main/java/com/example/curriculumapp/client/usuario/AuthClient.kt
@@ -3,16 +3,16 @@ package com.example.curriculumapp.client.usuario
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object UsuarioClient {
+object AuthClient {
     private const val BASE_URL = "http://10.0.2.2:8000/ms-usuarios"
 
-    val api: UsuarioApi by lazy {
+    val api: AuthApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(
                 GsonConverterFactory.create()
             )
             .build()
-            .create(UsuarioApi::class.java)
+            .create(AuthApi::class.java)
     }
 }
--- app/src/main/java/com/example/curriculumapp/client/usuario/UsuarioApi.kt
+++ app/src/main/java/com/example/curriculumapp/client/usuario/UsuarioApi.kt
@@ -5,27 +5,28 @@ import retrofit2.http.*
 
 interface UsuarioApi {
 
-    @GET("api/usuarios/{id}")
-    suspend fun findById(@Path("id") id: Long): UsuarioResponse
-
-    @PUT("api/usuarios/{id}")
-    suspend fun update(@Path("id") id: Long, @Body request: UpdateUsuarioRequest): UsuarioResponse
-
-    @DELETE("api/usuarios/{id}")
-    suspend fun delete(@Path("id") id: Long)
+    @POST("api/usuarios")
+    suspend fun criar(@Body request: CreateUsuarioRequest): UsuarioResponse
 
     @GET("api/usuarios")
-    suspend fun findAll(): List<UsuarioResponse>
-
-    @POST("api/usuarios")
-    suspend fun create(@Body request: CreateUsuarioRequest): UsuarioResponse
-
-    @PATCH("api/usuarios/senha/username")
-    suspend fun alterarSenhaPorUsername(@Body request: AlterarSenhaPorUsernameRequest)
-
-    @PATCH("api/usuarios/me/senha")
-    suspend fun alterarPropriaSenha(@Body request: AlterarSenhaRequest)
+    suspend fun listar(): List<UsuarioResponse>
 
     @GET("api/usuarios/me")
     suspend fun getMe(): UsuarioResponse
+
+    @GET("api/usuarios/{id}")
+    suspend fun buscarPorId(@Path("id") id: Long): UsuarioResponse
+
+    @PUT("api/usuarios/{id}")
+    suspend fun alterar(@Path("id") id: Long, @Body request: UpdateUsuarioRequest): UsuarioResponse
+
+    @DELETE("api/usuarios/{id}")
+    suspend fun deletar(@Path("id") id: Long)
+
+    @PATCH("api/usuarios/me/senha")
+    suspend fun alterarSenha(@Body request: AlterarSenhaRequest)
+
+    @PATCH("api/usuarios/senha/username")
+    suspend fun alterarSenhaPorEmail(@Body request: AlterarSenhaPorUsernameRequest)
+
 }
